00e2a1e2fdef846a77e660799c9dcccc2f76fde2
JoseBernal06 <150805766+JoseBernal06@users.noreply.github.com>
2025-06-29 22:59:35 -0500

    configuracion mongo atlas

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", true);
 const connection = async () => {
     try {
         const { connection } = await mongoose.connect(
-        process.env.MONGODB_URI_LOCAL
+        process.env.MONGODB_URI_ATLAS || process.env.MONGODB_URI_LOCAL 
         );
         console.log(
         `Database is connected on ${connection.host} - ${connection.port}`
