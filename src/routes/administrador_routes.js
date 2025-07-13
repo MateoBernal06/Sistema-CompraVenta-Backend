1921c414f5718556deb673e6d23335297ac2c8de
JoseBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2025-07-12 21:04:05 -0500

    arreglo rutas

--- src/routes/administrador_routes.js
+++ src/routes/administrador_routes.js
@@ -6,20 +6,21 @@ import {
     loginAdministrador,
     actualizarPassword,
     actualizarDatosAdministrador,
-    administradorPerfil
+    administradorPerfil,
+    recuperarPassword,
+    comprobarTokenPasword,
+    nuevoPassword
 } from "../controllers/administrador_controller.js";
 
-import {
-    recuperarPassword,comprobarTokenPasword,nuevoPassword
-} from "../controllers/estudiante_controller.js";
+
 
 router.post("/administrador/login", loginAdministrador);
 router.patch("/administrador/actualizar-password", verificarAutenticacion, actualizarPassword);
 router.patch("/administrador/actualizar-datos", verificarAutenticacion, actualizarDatosAdministrador);
 router.get("/administrador/perfil", verificarAutenticacion, administradorPerfil);
 
-router.post("/recuperar-password", recuperarPassword);
-router.get("/comprobar-token/:token", comprobarTokenPasword);
-router.post("/nuevo-password/:token", nuevoPassword);
+router.post("/administrador/recuperar-password", recuperarPassword);
+router.get("/administrador/comprobar-token/:token", comprobarTokenPasword);
+router.post("/administrador/nuevo-password/:token", nuevoPassword);
 
 export default router;
